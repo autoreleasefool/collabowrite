195503edffc26818ee8ba01e23feeeeabc5bba45
Joseph Roque <contact@josephroque.ca>
2017-11-19 01:36:54 -0500

    Authorization and WebSocket setup g Changes to be committed:

--- app/CollaboWrite/CollaboWrite/ViewControllers/ViewController.swift
+++ app/CollaboWrite/CollaboWrite/ViewControllers/ViewController.swift
@@ -9,35 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
+    let manager = ClientSocketManager()
+    let auth = Auth()
+
     @IBOutlet weak var SignUp: UIButton!
     @IBAction func takeToSignUpScreen(_ sender: UIButton) {
         let signUpScreen = self.storyboard!.instantiateViewController(withIdentifier: "SignUpViewController")
         self.present(signUpScreen, animated: true, completion: nil)
     }
-    
+
     @IBOutlet weak var getIn: UIButton!
     @IBAction func takeToGetIn(_ sender: UIButton) {
         let signUpScreen = self.storyboard!.instantiateViewController(withIdentifier: "getInViewController")
         self.present(signUpScreen, animated: true, completion: nil)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.openSocketConnection()
+
+        auth.delegate = self
+        auth.attemptLogIn(username: "un", password: "pw")
     }
-    
-        // Do any additional setup after loading the view, typically from a nib.
-        /* for family: String in UIFont.familyNames {
-         print("\(family)")
-         for names: String in UIFont.fontNames(forFamilyName:) {
-         print("== \(names)")
-         }
-         }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: AuthDelegate {
+
+    func onAuthSuccess(userId: String) {
+        print("Got user id: \(userId)")
+    }
+}
